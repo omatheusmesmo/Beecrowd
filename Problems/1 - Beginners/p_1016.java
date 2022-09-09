b2669c03b0195f90c07c5081841348af1d25ccba
Matheus Oliveira <matheus.6148@gmail.com>
2022-09-09 20:17:29 -0300

    1016 inicio

---
+++ Problems/1 - Beginners/p_1016.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class p_1016 {
+    public static void main(String[] args) {
+        int km, tempo;
+        Scanner sc = new Scanner(System.in);
+
+        km = sc.nextInt();
+
+        tempo = (km*1000/90*1000)
+
+
+        sc.close();
+    }
+}
