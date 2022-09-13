9c63da8b15217d38049a7e8738cbc7f1a14fb9b6
Matheus Oliveira <matheus.6148@gmail.com>
2022-09-13 18:40:52 -0300

    1019 resolvido

--- Problems/1 - Beginners/p_1019.java
+++ Problems/1 - Beginners/p_1019.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class p_1019 {
-    
+    public static void main(String[] args){
+        int N, horas, minutos, segundos;
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        horas = N/3600;
+        N %= 3600;
+        minutos = N/60;
+        N %= 60;
+        segundos = N/1;
+
+        System.out.printf("%d:%d:%d", horas,minutos, segundos);
+
+        sc.close();
+    }
 }
