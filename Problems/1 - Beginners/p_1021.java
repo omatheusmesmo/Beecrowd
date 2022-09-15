b3721abc5377ff46e9228273f2c07d182a261d7b
Matheus Oliveira <matheus.6148@gmail.com>
2022-09-14 21:19:15 -0300

    1021 identado

--- Problems/1 - Beginners/p_1021.java
+++ Problems/1 - Beginners/p_1021.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class p_1021 {
     public static void main(String[] args) {
-        float entrada;
+        double entrada;
         int n100, n50, n20, n10, n5, n2, m100, m50, m25, m10, m05, m01;
 
         Scanner sc = new Scanner(System.in);
@@ -11,20 +11,31 @@ public class p_1021 {
         //contagem notas
         n100 = (int) (entrada/100);
         entrada %= 100;
+        System.out.printf("%d notas de 100\n", n100);
+
         n50 =  (int) (entrada/50);
         entrada %= 50;
+        System.out.printf("%d notas de 100\n", n50);
+
         n20 =  (int) entrada/20;
         entrada %= 20;
+        System.out.printf("%d notas de 100\n", n20);
+
         n10 =  (int) entrada/10;
         entrada %= 10;
+        System.out.printf("%d notas de 100\n", n10);
+        
         n5 =  (int) entrada/5;
         entrada %= 5;
+        System.out.printf("%d notas de 100\n", n5);
+
         n2 =  (int) entrada/2;
         entrada %= 2;
+        System.out.printf("%d notas de 100\n", n2);
+
         //contagem moeda
         m100 = (int) (entrada/1);
 
-        System.out.printf("%d notas de 100\n %d notas de 50\n %d notas de 20\n %d notas de 10\n %d notas de 5\n %d notas de 2\n", n100, n50, n20, n10, n5, n2);
 
         sc.close();
     }
